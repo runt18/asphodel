8756c082bc6937b6077a918580b5adc875b9adb4
Alistair Wick <alistair.wk@gmail.com>
2013-03-02 18:40:07 +0000

    Crude mesh loading (DAE)

--- src/com/heisenberg/asphodel/GameData.java
+++ src/com/heisenberg/asphodel/GameData.java
@@ -1,12 +1,22 @@
 package com.heisenberg.asphodel;
 
+import java.util.ArrayList;
+
 public class GameData {
     private static boolean initialised = false;
+    
+    // Data
+    private static ArrayList<Mesh> meshes;
     
     static void doInitialisation() {
         if (!initialised) {
             // Put initialisation code here
             TextureManager.init();
+            meshes = new ArrayList<Mesh>();
+            
+            // Try making a mesh
+            Mesh m = new Mesh(R.raw.test_mesh);
+            meshes.add(m);
             
             initialised = true;
         }
